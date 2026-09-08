401cf9d9ba74691e351a89a55a17b88e1a4fd653
SudishKumar <sudish.kumar@bigul.co>
2026-09-08 17:52:54 +0530

    Update README, package.json, and gatsby-config metadata for Sudish Kumar

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
     title: 'Sudish Kumar',
     description:
       'Sudish Kumar is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://github.com/sudishk/sudish_kumar', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@sudishk',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
